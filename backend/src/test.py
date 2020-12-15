285c2a787206e8daae4a73c187f5de24c7a66bce
OmarThinks <contact.parmag@gmail.com>
2020-12-15 19:35:50 +0200

    test:mod:renamed test functions
    to match the new naming convention of the of the
    testing functions
    the convention that i have inplemented to organize
    tetsing functions names

--- backend/src/test.py
+++ backend/src/test.py
@@ -54,43 +54,43 @@ class TriviaTestCase(unittest.TestCase):
 		print("Test 2: db_drop_and_create_all")
 
 
-	def test_a_a_001_product_insert(self):
+	def test_a_1_001_product_insert(self):
 		product1 = Product(name = "product1",price = 5.5,
 			in_stock=True, seller=1)
 		product1.insert()
 		products = Product.query.all()
 
 		self.assertEqual(len(products),1)
-		print("Test a_a_1: Product insert")
+		print("Test a_1_1: Product insert")
 
 
-	def test_a_a_002_product_update(self):
+	def test_a_1_002_product_update(self):
 		product1 = Product.query.get(1)
 		product1.name = "modified"
 		product_1 = Product.query.get(1)
 
 		self.assertEqual(product_1.name,"modified")
-		print("Test a_a_2: Product update")
+		print("Test a_1_2: Product update")
 
 
 
-	def test_a_a_003_product_delete(self):
+	def test_a_1_003_product_delete(self):
 		product1 = Product.query.get(1)
 		product1.delete()
 		products = Product.query.all()
 
 		self.assertEqual(len(products),0)
-		print("Test a_a_3: Product delete")
+		print("Test a_1_3: Product delete")
 
-	def test_a_a_004_populate(self):
+	def test_a_1_004_populate(self):
 		populate_tables()
 		products = Product.query.all()
 
 		self.assertEqual(len(products),6)
-		print("Test a_a_4: Populate Tables")
+		print("Test a_1_4: Populate Tables")
 
 
-	def test_a_a_005_product_values(self):
+	def test_a_1_005_product_values(self):
 		produc = Product.query.get(1)
 
 		self.assertEqual(produc.id,1)
@@ -98,10 +98,10 @@ class TriviaTestCase(unittest.TestCase):
 		self.assertEqual(produc.price,300)
 		self.assertEqual(produc.seller,1)
 		self.assertEqual(produc.in_stock,True)
-		print("Test a_a_5: Product values")
+		print("Test a_1_5: Product values")
 
 
-	def test_a_a_006_product_insert_wrong(self):
+	def test_a_1_006_product_insert_wrong(self):
 		products = Product.query.all()
 		old_records_number = len(products)
 		try:
@@ -118,12 +118,12 @@ class TriviaTestCase(unittest.TestCase):
 
 		self.assertEqual(old_records_number,
 			new_records_number)
-		print("Test a_a_6: product insert with missing"+
+		print("Test a_1_6: product insert with missing"+
 		 "required parameters")
 
 
 
-	def test_a_a_007_product_delete_wrong(self):
+	def test_a_1_007_product_delete_wrong(self):
 		products = Product.query.all()
 		old_records_number = len(products)
 		try:
@@ -141,21 +141,21 @@ class TriviaTestCase(unittest.TestCase):
 
 		self.assertEqual(old_records_number,
 			new_records_number)
-		print("Test a_a_7: product delete mistake, non-existent"+
+		print("Test a_1_7: product delete mistake, non-existent"+
 		 "product id")
 
 
 
 
-	def test_a_a_008_get_in_stock_products(self):
+	def test_a_1_008_get_in_stock_products(self):
 		products = get_in_stock_products()
 		for product in products:
 			self.assertEqual(product.in_stock,True)
-		print("Test a_a_8:get in stock products")
+		print("Test a_1_8:get in stock products")
 
 
 
-	def test_a_a_009_product_simple(self):
+	def test_a_1_009_product_simple(self):
 		produc = Product.query.get(1).simple()
 		#print(produc)
 
@@ -174,7 +174,7 @@ class TriviaTestCase(unittest.TestCase):
 		self.assertEqual(produc["in_stock"],True)
 		self.assertEqual(type(produc["in_stock"]),bool)
 
-		print("Test a_a_9: Product simple")
+		print("Test a_1_9: Product simple")
 
 
 
