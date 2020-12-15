a8d48f1fd86f6ae1bd06e123f84cea9944e18b5f
OmarThinks <contact.parmag@gmail.com>
2020-12-15 09:38:27 +0200

    database directory updated
    now the databases are located in the databases directory
    so i have moved the indcation of the db to this place

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,5 +3,5 @@ SECRET_KEY=os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
-	os.path.join(os.path.dirname(os.path.abspath(__file__)), "database.sqlite"))
+	os.path.join(os.path.dirname(os.path.abspath(__file__)), "databases/database.sqlite"))
 SQLALCHEMY_TRACK_MODIFICATIONS= False
