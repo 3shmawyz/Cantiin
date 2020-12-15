54bff7db18f2965159d5f4fa46ec614dea952144
OmarThinks <contact.parmag@gmail.com>
2020-12-15 19:39:27 +0200

    test:mod:copied product test to order
    this is a step to prepare to test the order model

--- backend/src/test.py
+++ backend/src/test.py
@@ -188,6 +188,147 @@ class TriviaTestCase(unittest.TestCase):
 
 
 
+
+
+
+
+
+
+
+	def test_a_2_001_product_insert(self):
+		product1 = Product(name = "product1",price = 5.5,
+			in_stock=True, seller=1)
+		product1.insert()
+		products = Product.query.all()
+
+		self.assertEqual(len(products),1)
+		print("Test a_1_1: Product insert")
+
+
+	def test_a_2_002_product_update(self):
+		product1 = Product.query.get(1)
+		product1.name = "modified"
+		product_1 = Product.query.get(1)
+
+		self.assertEqual(product_1.name,"modified")
+		print("Test a_1_2: Product update")
+
+
+
+	def test_a_2_003_product_delete(self):
+		product1 = Product.query.get(1)
+		product1.delete()
+		products = Product.query.all()
+
+		self.assertEqual(len(products),0)
+		print("Test a_1_3: Product delete")
+
+	def test_a_2_004_populate(self):
+		populate_tables()
+		products = Product.query.all()
+
+		self.assertEqual(len(products),6)
+		print("Test a_1_4: Populate Tables")
+
+
+	def test_a_2_005_product_values(self):
+		produc = Product.query.get(1)
+
+		self.assertEqual(produc.id,1)
+		self.assertEqual(produc.name,"Labtop")
+		self.assertEqual(produc.price,300)
+		self.assertEqual(produc.seller,1)
+		self.assertEqual(produc.in_stock,True)
+		print("Test a_1_5: Product values")
+
+
+	def test_a_2_006_product_insert_wrong(self):
+		products = Product.query.all()
+		old_records_number = len(products)
+		try:
+			#This code will not be executed
+			#There are missing required parameters
+			product1 = Product()
+			product1.insert()
+			self.assertEqual(True,False)
+		except:
+			self.assertEqual(True,True)
+		
+		products = Product.query.all()
+		new_records_number = len(products)
+
+		self.assertEqual(old_records_number,
+			new_records_number)
+		print("Test a_1_6: product insert with missing"+
+		 "required parameters")
+
+
+
+	def test_a_2_007_product_delete_wrong(self):
+		products = Product.query.all()
+		old_records_number = len(products)
+		try:
+			#This code will not be executed
+			#There is no product with the number 0
+			product1 = Product.query.get(0)
+			product1.delete()
+			self.assertEqual(True,False)
+
+		except:
+			self.assertEqual(True,True)
+		
+		products = Product.query.all()
+		new_records_number = len(products)
+
+		self.assertEqual(old_records_number,
+			new_records_number)
+		print("Test a_1_7: product delete mistake, non-existent"+
+		 "product id")
+
+
+
+
+	def test_a_2_008_get_in_stock_products(self):
+		products = get_in_stock_products()
+		for product in products:
+			self.assertEqual(product.in_stock,True)
+		print("Test a_1_8:get in stock products")
+
+
+
+	def test_a_2_009_product_simple(self):
+		produc = Product.query.get(1).simple()
+		#print(produc)
+
+		self.assertEqual(produc["id"],1)
+		self.assertEqual(type(produc["id"]),int)
+
+		self.assertEqual(produc["name"],"Labtop")
+		self.assertEqual(type(produc["name"]),str)
+
+		self.assertEqual(produc["price"],300)
+		self.assertEqual(type(produc["price"]),float)
+
+		self.assertEqual(produc["seller"],1)
+		self.assertEqual(type(produc["seller"]),int)
+
+		self.assertEqual(produc["in_stock"],True)
+		self.assertEqual(type(produc["in_stock"]),bool)
+
+		print("Test a_1_9: Product simple")
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Make the tests conveniently executable
 if __name__ == "__main__":
 	unittest.main()
