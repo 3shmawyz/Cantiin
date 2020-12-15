8b5c6aaf80052728cfb551b85e0287dc2bce46b4
OmarThinks <contact.parmag@gmail.com>
2020-12-15 17:14:43 +0200

    test:refractor: edited test functions names
    the test functions names had additional test name
    so i have removed this redundant

--- backend/src/test.py
+++ backend/src/test.py
@@ -34,12 +34,12 @@ class TriviaTestCase(unittest.TestCase):
 		print("_+++++++++++++++++++++++++++++++++_")
 
 	#Note: Tests are run alphapetically
-	def test_001_test_test(self):
+	def test_001_test(self):
 		self.assertEqual(1,1)
 		print("Test 1:Hello, Tests!")
 
 
-	def test_003_test_drop_all_create_all(self):
+	def test_003_drop_all_create_all(self):
 		db_drop_and_create_all()
 		products = Product.query.all()
 
@@ -47,7 +47,7 @@ class TriviaTestCase(unittest.TestCase):
 		print("Test 2: db_drop_and_create_all")
 
 
-	def test_003_test_product_insert(self):
+	def test_003_product_insert(self):
 		product1 = Product(name = "product1",price = 5.5,
 			in_stock=True, seller=1)
 		product1.insert()
@@ -57,7 +57,7 @@ class TriviaTestCase(unittest.TestCase):
 		print("Test 3: Product insert")
 
 
-	def test_004_test_product_update(self):
+	def test_004_product_update(self):
 		product1 = Product.query.get(1)
 		product1.name = "modified"
 		product_1 = Product.query.get(1)
@@ -67,7 +67,7 @@ class TriviaTestCase(unittest.TestCase):
 
 
 
-	def test_005_test_product_delete(self):
+	def test_005_product_delete(self):
 		product1 = Product.query.get(1)
 		product1.delete()
 		products = Product.query.all()
@@ -75,7 +75,7 @@ class TriviaTestCase(unittest.TestCase):
 		self.assertEqual(len(products),0)
 		print("Test 5: Product delete")
 
-	def test_006_test_populate(self):
+	def test_006_populate(self):
 		populate_tables()
 		products = Product.query.all()
 
@@ -83,7 +83,7 @@ class TriviaTestCase(unittest.TestCase):
 		print("Test 6: Populate Tables")
 
 
-	def test_007_test_product_values(self):
+	def test_007_product_values(self):
 		produc = Product.query.get(1)
 
 		self.assertEqual(produc.id,1)
@@ -94,6 +94,24 @@ class TriviaTestCase(unittest.TestCase):
 		print("Test 7: Product values")
 
 
+	def test_008_product_insert_wrong(self):
+		products = Product.query.all()
+		old_records_number = len(products)
+		try:
+			#This code will not be executed
+			#There are missing required parameters
+			product1 = Product()
+			product1.insert()
+		except:
+			pass
+		
+		products = Product.query.all()
+		new_records_number = len(products)
+
+		self.assertEqual(old_records_number,
+			new_records_number)
+		print("Test8: product insert with missing"+
+		 "required parameters")
 
 
 
