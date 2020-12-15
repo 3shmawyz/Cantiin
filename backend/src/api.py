c47e5f3bac531b39d3834b8b927f5f5011d664d0
OmarThinks <contact.parmag@gmail.com>
2020-12-15 09:20:10 +0200

    api.py:mod: filled with lines

---
+++ backend/src/api.py
@@ -0,0 +1,167 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_cors import CORS
+from flask_migrate import Migrate 
+from flask_sqlalchemy import SQLAlchemy
+from random import shuffle
+import json
+from models import Question, Category, db, populate_tables, clear_tables
+from random import shuffle
+
+"""
+Routes:
+	1)	"/populate" ->--------->"GET" , "OPTIONS"
+	2)	"/clear_tables"-------->"GET" , "OPTIONS"
+
+"""
+
+
+
+
+QUESTIONS_PER_PAGE = 10
+
+
+def paginate_questions(questions_list,pagination):
+	#This function will return a 
+	#(Paginated, fomatted) list of questions
+	min_index=(pagination-1) * QUESTIONS_PER_PAGE
+	max_index=(pagination) * QUESTIONS_PER_PAGE
+	paginated_formatted_questions_list = list()
+	for index,question in enumerate(questions_list):
+		if index >= min_index:
+			if index < max_index:
+				paginated_formatted_questions_list.append(
+					question.format())
+	return paginated_formatted_questions_list
+
+
+
+
+
+
+"""
+This method searches inside The question model.
+
+Input: String to be searched
+Output: Fomatted list of questions matching the search
+"""
+def question_search(input_text):
+	search_query = input_text.strip()
+	#To remove the spqce from the beginning and the end of string
+	search_query = "%"+search_query+"%"
+	all_questions = db.session.query(Question).filter(
+		Question.question.ilike(search_query)).all()
+	to_return = [question.format() for question in all_questions]
+	return to_return
+
+
+
+def create_app(test_config=None):
+	# create and configure the app
+	app = Flask(__name__)
+	#db=SQLAlchemy(app)
+	app.config.from_object("config")
+	db.app = app
+	migrate = Migrate(app,db)
+	db.init_app(app)
+	#populate_tables()
+	CORS(app,resources={r"*":{"origins":"*"}})
+	@app.after_request
+	def after_request(response):
+		response.headers.add("Access-Control-allow-Origin","*")
+
+		response.headers.add("Access-Control-allow-Headers",
+			"Content-Type,Autorization,true")
+		response.headers.add("Access-Control-allow-Methods",
+			"GET,PUT,POST,DELETE,OPTIONS")
+
+		return response
+		
+
+
+	@app.route("/populate", methods=["GET"])
+	def populate_all_tables():
+		#This endpoint will clear all the data in the database and 
+		#populate with new data
+		try:
+			populate_tables()
+			return jsonify({"success":True})
+		except:
+			abort(422) #Unprocessible
+		"""
+Tests: test_01_clear_tables
+		"""
+		
+
+	@app.route("/clear_tables", methods=["GET"])
+	def clear_all_tables():
+		clear_tables()
+		"""
+Tests: test_02_populate_test
+		"""
+		return jsonify({"success":True})
+
+
+
+
+
+
+	def return_error(id):
+		if id==1:
+			return jsonify({"success":False,"error":400,
+			"message":"bad request",
+			"details":"missing request body"
+			}),400
+		if id==2:
+			return jsonify({"success":False,"error":400,
+			"message":"bad request",
+			"details":"missing required variables in request body"
+			}),400
+		if id==3:
+			return jsonify({"success":False,"error":422,
+			"message":"unprocessible",
+			"details":"there is a mistake in data types"
+			}),422
+		if id==4:
+			return jsonify({"success":False,"error":422,
+			"message":"unprocessible",
+			"details":"this category id is not in the database"
+			}),422			
+
+		if id==5:
+			return jsonify({"success":False,"error":422,
+			"message":"unprocessible",
+			"details":"this question id is not in the database"
+			}),422			
+
+
+
+	
+
+
+
+
+
+	@app.errorhandler(404)
+	def not_found(error):
+		return jsonify({"success":False,"error":404,
+			"message":"resource not found"}),404
+
+	@app.errorhandler(422)
+	def unprocessible(error):
+		return jsonify({"success":False,"error":422,
+			"message":"unprocessible"}),422
+
+
+	@app.errorhandler(400)
+	def bad_request(error):
+		return jsonify({"success":False,"error":400,
+			"message":"bad request"}),400
+
+
+	@app.errorhandler(405)
+	def method_not_allowed(error):
+		return jsonify({"success":False,"error":405,
+			"message":"method not allowed"}),405
+	
+	return app		
