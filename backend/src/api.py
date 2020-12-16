fdcd2286919911410d5ab0d7f01a5933184ee9ee
OmarThinks <contact.parmag@gmail.com>
2020-12-16 21:51:17 +0200

    api:mod:imported models: product and order

--- backend/src/api.py
+++ backend/src/api.py
@@ -5,7 +5,8 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from random import shuffle
 import json
-from models import db, populate_tables, db_drop_and_create_all
+from models import (db, populate_tables, 
+	db_drop_and_create_all,Product, Order)
 from random import shuffle
 
 """
