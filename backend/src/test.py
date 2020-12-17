cf75609a373a32061ecb657da76bdf4138f1c671
OmarThinks <contact.parmag@gmail.com>
2020-12-17 19:25:45 +0200

    fixing imports

--- backend/src/test.py
+++ backend/src/test.py
@@ -8,9 +8,7 @@ from sqlalchemy import func
 
 from api import create_app
 from models import (db,Product, Order)
-from functions import (populate_tables,db_drop_and_create_all
-	, get_in_stock_products,validate_product_name,
-	validate_product_price)
+from functions import *
 from flask_cors import CORS
 from flask_migrate import Migrate 
 from flask_sqlalchemy import SQLAlchemy
