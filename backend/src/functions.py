e2c363694ab527360af68c8547070c1d0402b5c2
OmarThinks <contact.parmag@gmail.com>
2020-12-17 16:14:05 +0200

    functions: starting to validate the order inputs

--- backend/src/functions.py
+++ backend/src/functions.py
@@ -204,6 +204,153 @@ def validate_product_seller(input_s):
 
 
 
+def validate_product_exists(input_id,all_products):
+	#Validate that product id has a value, not None
+	if input_id == None: return [False,None]
+	
+	#Validate that product id can be converted to float
+	try:
+		id = int(input_id)
+	except:
+		return [False,my_error(status=400, 
+			description="id can not be converted to integer")]
+	
+	#Validate that product id is not negative or zero
+	if id<=0:
+		return [False,my_error(status=422, 
+			description="id can not be less than"+
+			" or equal to 0")]
+
+	#There should be a code to validate that the id
+	#Is in the users table
+	try:
+		product = all_products.get(id)
+	except Exception as e:
+		return [False,my_error(status=422, 
+			description="there is no product with this id")]
+	if product == None :
+		return [False,my_error(status=422, 
+			description="there is no product with this id")]
+
+	return [True,product]
+
+
+
+
+
+
+
+
+def validate_product_name(input_n #,all_products
+	):
+	#Validate that product name has a value, not None
+	if input_n == None: return [True,None]
+	
+	#Validate that product name can be converted to string
+	try:
+		name = str(input_n)
+	except:
+		return [False,my_error(status=400, 
+			description="name can not be converted to string")]
+	
+	#Validate that product name length is less that 100
+	if len(name)>100:
+		return [False,my_error(status=422, 
+			description="maximum name length is 100 letters")]
+
+	#Validating that there is no product wth this name already
+	"""all_products_names = [p.name.strip().casefold(
+		) for p in all_products]
+	print(all_products_names,flush=True)
+	if name.strip().casefold() in all_products_names:
+		return [False,my_error(status=422, 
+			description="there is a product"+
+			" with this name already")]"""
+
+	return [True,name]
+
+
+
+
+
+
+
+
+def validate_product_price(input_p):
+	#Validate that product price has a value, not None
+	if input_p == None: return [True,None]
+	
+	#Validate that product price can be converted to float
+	try:
+		price = float(input_p)
+	except:
+		return [False,my_error(status=400, 
+			description="price can not be converted to float")]
+	
+	#Validate that product price is not negative or zero
+	if price<=0:
+		return [False,my_error(status=422, 
+			description="price can not be less than"+
+			" or equal to 0")]
+
+	return [True,price]
+
+
+
+
+def validate_product_in_stock(input_i):
+	#Validate that product in_stock has a value, not None
+	if input_i == None: return [True,None]
+	
+	#Validate that product in_stock can be converted to boolean
+	try:
+		in_stock = bool(input_i)
+	except:
+		return [False,my_error(status=400, 
+			description="in_stock can not be "+
+			"converted to boolean")]
+
+	return [True,in_stock]
+
+
+
+
+
+
+def validate_product_seller(input_s):
+	#Validate that product seller has a value, not None
+	if input_s == None: return [True,None]
+	
+	#Validate that product seller can be converted to float
+	try:
+		seller = int(input_s)
+	except:
+		return [False,my_error(status=400, 
+			description="seller can not be converted to integer")]
+	
+	#Validate that product seller is not negative or zero
+	if seller<=0:
+		return [False,my_error(status=422, 
+			description="seller can not be less than"+
+			" or equal to 0")]
+
+	#There should be a code to validate that the seller
+	#Is in the users table
+	return [True,seller]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def db_drop_and_create_all():
     db.drop_all()
