180d668a0e3d60e5f4d5f298aa458df549ecaedb
OmarThinks <contact.parmag@gmail.com>
2020-12-20 18:56:40 +0200

    functions:add:my_error:message

--- backend/functions.py
+++ backend/functions.py
@@ -37,11 +37,19 @@ def my_error(status=404 ,description=""):
 	if status not in [400,401,403,404,405,422,500]:
 		raise Exception("status is "+str(status)
 			+ ", not in [[400,401,403,404,405,422,500]]")
-	
+	if status == 400: message = "bad request"
+	elif status == 401: message = "unauthorized"
+	elif status == 403: message = "forbidden"
+	elif status == 404: message = "not found"
+	elif status == 405: message = "method not allowed"
+	elif status == 422: message = "unprocessible"
+	else : message = "internal server error"
+
+
 	error_dict = {
 					"success": False, 
 					"error": status,
-					"message": "Not Found",
+					"message": message,
 					}
 
 	if description == "":
