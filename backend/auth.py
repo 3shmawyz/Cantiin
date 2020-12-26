fa872f3e877a1bf7b3885981e8c941b7f50bc5eb
OmarThinks <contact.parmag@gmail.com>
2020-12-26 19:33:06 +0200

    auth:add:prepare:get_token_auth_cookie

--- backend/auth.py
+++ backend/auth.py
@@ -215,6 +215,70 @@ def validate_token(token,secret):
 
 
 
+def get_token_auth_cookie():
+    """Obtains the Access Token from the Authorization Header
+    """
+   
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        
+        abort(401)
+        raise AuthError({"code": "authorization_header_missing",
+                        "description":
+                            "Authorization header is expected"}, 401)
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        abort(401)
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must start with"
+                            " Bearer"}, 401)
+    elif len(parts) == 1:
+        abort(401) # No token Sent
+        raise AuthError({"code": "invalid_header",
+                        "description": "Token not found"}, 401)
+    elif len(parts) > 2:
+        abort(401)
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must be"
+                            " Bearer token"}, 401)
+
+    token = parts[1]
+    return token
+
+
+
+
+
+
+
+
+
+
+def requires_auth():
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            #return f(*args, **kwargs)
+            token = get_token_auth_header()
+            payload = verify_decode_jwt(token)
+            check_permissions(permission, payload)
+            return f(#payload, 
+                *args, **kwargs)
+
+        return wrapper
+    return requires_auth_decorator
+
+
+
+
+
+
+
+
 
 
 
@@ -334,23 +398,6 @@ def check_permissions(permission, payload):
 
 
 
-
-
-
-
-def requires_auth():
-    def requires_auth_decorator(f):
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            #return f(*args, **kwargs)
-            token = get_token_auth_header()
-            payload = verify_decode_jwt(token)
-            check_permissions(permission, payload)
-            return f(#payload, 
-                *args, **kwargs)
-
-        return wrapper
-    return requires_auth_decorator
 
 
 
