b0193122ab51ee152d3b673eb05173e4eb5f61a5
OmarThinks <contact.parmag@gmail.com>
2020-12-30 17:47:58 +0200

    now there is the src folder only

--- frontend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-import secrets
-#SECRET_KEY=os.urandom(32)
-SECRET_KEY=secrets.token_urlsafe(5000)
-basedir = os.path.abspath(os.path.dirname(__file__))
-# Enable debug mode.
-DEBUG = True
-
-SQLALCHEMY_TRACK_MODIFICATIONS= False
