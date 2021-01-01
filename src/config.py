07fcef0b61f8d97f393a93d1316e0317348f518d
OmarThinks <contact.parmag@gmail.com>
2021-01-01 22:28:08 +0200

    removed config files
    now they are environmental variables

--- src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-import secrets
-#SECRET_KEY=os.urandom(32)
-SECRET_KEY=secrets.token_urlsafe(5000)
-basedir = os.path.abspath(os.path.dirname(__file__))
-#DEBUG = True
-SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
-	os.path.join(os.path.dirname(os.path.abspath(__file__)), "databases/database.sqlite"))
-SQLALCHEMY_TRACK_MODIFICATIONS= False
