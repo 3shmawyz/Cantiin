86512ef5eddb7db7f85a6ea27b8f3488c7397bc7
OmarThinks <contact.parmag@gmail.com>
2021-01-14 19:49:18 +0200

    functions.js:add:create_manage_product_card

--- src/static/js/functions.js
+++ src/static/js/functions.js
@@ -80,13 +80,52 @@ function home_product_card(input_product)
     product_name+'</h5>'+
     '<p class="card-text">Price : <b>'+
     product_price+'</b></p>'+
-    '<div class="d-flex"><a target="_blank" class="stretched-link ml-auto btn btn-outline-success " href="product?id='
+    '<div class="d-flex"><a target="_blank" '+
+    'class="stretched-link ml-auto btn btn-outline-success " href="product?id='
     +product_id+'"'+ 
     'class="">'+right_arrow_svg+'</a>'+
     '</div>'+
   '</div>'+
   '</div>'
 }
+
+
+
+function create_manage_product_card(input_product)
+{
+  product_name=input_product["name"];
+  product_id = input_product["id"];
+  product_price = input_product["price"];
+  return '<div class="card mb-4" style="">'+
+  '  <div class="card-body">'+
+  '    <h5 class="card-title"'+ 
+  '    style="font-weight: bold; '+
+        'font-size: 150%;">'+product_name+'</h5>'+
+  '    <p class="card-text">Price :  <b>'+product_price+'</b></p>'+
+  '    <div class="d-flex">'+
+  '      <a href="edit-product?id='+product_id+
+  '" target="_blank" '+
+  '       class=" card_button btn btn-outline-warning" '+
+  '      style="font-weight: bold;font-size: '+
+  '          150%; color: black;">'+
+  '        Edit'+
+  '      </a>'   +   
+
+  '      <a target="_blank" '+
+  '       class="ml-auto card_button btn btn-outline-danger"'+ 
+  '      style="font-weight: bold;font-size: 150%"'+
+  '      data-toggle="modal" data-target="#exampleModal"'+
+  '      onclick="delete_product('+product_id+')" '+
+  '      >DELETE'+
+  '      </a> '+
+  '    </div>'+
+  '  </div>'+
+  '</div>'
+}
+
+
+
+
 
 
 
