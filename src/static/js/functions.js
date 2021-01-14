0229926950b8aaef791c0af049a6bf101d0bceee
OmarThinks <contact.parmag@gmail.com>
2021-01-14 19:44:23 +0200

    functions.js: moved cards to functions.js

--- src/static/js/functions.js
+++ src/static/js/functions.js
@@ -42,3 +42,65 @@ function signTheUserOut()
 changeHeaderIfYouSHould();
 
 
+
+
+
+
+
+
+
+/*
+Cards Templates
+*/
+
+/*
+product_example=
+{
+    "id": 1,
+    "in_stock": true,
+    "name": "Labtop",
+    "price": 300.0,
+    "seller_id": 1
+}
+*/
+
+
+/*
+This is the card of the product, created from the product itself 
+*/
+function home_product_card(input_product)
+{
+  product_name=input_product["name"];
+  product_id = input_product["id"];
+  product_price = input_product["price"];
+  return '<div class="card mb-4">'+
+  '<div class="card-body">'+
+    '<h5 class="card-title"'+
+    'style="font-weight: bold; font-size: 150%;">'+
+    product_name+'</h5>'+
+    '<p class="card-text">Price : <b>'+
+    product_price+'</b></p>'+
+    '<div class="d-flex"><a target="_blank" class="stretched-link ml-auto btn btn-outline-success " href="product?id='
+    +product_id+'"'+ 
+    'class="">'+right_arrow_svg+'</a>'+
+    '</div>'+
+  '</div>'+
+  '</div>'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
