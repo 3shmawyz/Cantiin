c697ab1d17adc640f4a506ee812b1d2b6051800c
OmarThinks <contact.parmag@gmail.com>
2021-01-17 11:58:57 +0200

    cantiin.py:self documentation

--- cantiin.py
+++ cantiin.py
@@ -1,5 +1,20 @@
 from src import create_app
 application = create_app(testing=True)
 
+
+"""
+Note: The orginal application is packaged inside src directory
+You can find the real application there:
+
+src/app.py
+
+When to use this file?
+Run this file in testing on local host.
+
+python cantiin.py
+
+"""
+
+
 if __name__ == '__main__':
 	application.run()
