6cd246258c667ebe0c625a0c2560ecd8c78aadec
OmarThinks <contact.parmag@gmail.com>
2021-01-17 12:00:52 +0200

    unit_test.py:self documentation

--- unit_tests.py
+++ unit_tests.py
@@ -1,5 +1,20 @@
 import unittest
 from src import *
 
+
+"""
+Note: The orginal unittest is packaged inside src directory
+You can find the real unittest there:
+
+src/test.py
+
+How to use this file?
+
+python unit_test.py
+
+"""
+
+
+
 if __name__ == '__main__':
 	unittest.main()
