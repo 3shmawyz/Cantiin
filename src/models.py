aaeaf265e55e87ae1db009c4b16aac27b0dd3b91
OmarThinks <contact.parmag@gmail.com>
2021-01-17 21:29:04 +0200

    models.py:mod:Image.simple

--- src/models.py
+++ src/models.py
@@ -391,7 +391,10 @@ class Image(db.Model):
     def simple(self):
         return {
             'id': self.id,
-            'username': self.username
+            'seller_id': self.seller_id,
+            'name': self.name,
+            'formatting': self.formatting,
+            'stored_name': self.stored_name
         }
 
     def get_dict(self):
