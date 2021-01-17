823a7876e301e445e3e0ce54b0106cdfb229b026
OmarThinks <contact.parmag@gmail.com>
2021-01-17 20:49:42 +0200

    app.py:created manage images endpoint

--- src/app.py
+++ src/app.py
@@ -142,6 +142,10 @@ def create_app(DOCKER=False,testing=TESTING):
 	def manage_products():
 		return render_template('pages/manage_products.html')
 
+	@app.route('/manage-imagess', methods=['GET'])
+	def manage_images():
+		return render_template('pages/manage_images.html')
+
 	@app.route('/product', methods=['GET'])
 	def product():
 		return render_template('pages/product.html')
