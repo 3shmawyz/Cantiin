02eef8f8ceb2ac951bae7e4449b5d7f31aa27dff
OmarThinks <contact.parmag@gmail.com>
2021-01-30 18:54:08 +0200

    re_models.py:add:db init

--- resrc/re_models.py
+++ resrc/re_models.py
@@ -4,10 +4,25 @@ from sqlalchemy.ext.declarative import declarative_base
 import json
 
 
-try:
-    from __init__ import db
-except:
-    from src import db
+
+
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine('sqlite:///database.sqlite3', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+
+
+
 
 
 class NotReceived():
