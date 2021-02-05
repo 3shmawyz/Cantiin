67d8f56f37200af7c49923e3d3463a96a36501c4
OmarThinks <contact.parmag@gmail.com>
2021-02-06 01:57:53 +0200

    database.js:add:connect to postgres

--- node/database.js
+++ node/database.js
@@ -1,7 +1,9 @@
-var  Sequelize = require('sequelize');
+var  {Sequelize, DataTypes} = require('sequelize');
 // Option 1: Passing a connection URI
 const connection = new Sequelize(
-'postgresql://postgres:Rq5mVTej7Zb4xY@localhost:5432/cantiin_node'); 
+'postgresql://postgres:Rq5mVTej7Zb4xY@localhost:5432/cantiin_node',
+{  logging:false
+}); 
 
 
 
@@ -10,6 +12,37 @@ sequelize.org -> documentation -> getting started
 */
 
 
+const User = connection.define('User', 
+{
+  // Model attributes are defined here
+  // id is already defined
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    //defaultValue:""
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+    // allowNull defaults to true
+  }
+}, {
+  timestamps:false
+});
+
+
+/*
+
+var User = connection.define("user",
+	{
+		"id": Sequelize.INTEGER({'primaryKey': true}),
+		"username": Sequelize.STRING,
+		"password": Sequelize.STRING
+	}
+)
+
+*/
 
 
 
