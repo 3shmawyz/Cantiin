c64a400313d8f4a96004df31f214d0f0c0ec863a
OmarThinks <contact.parmag@gmail.com>
2021-02-07 12:06:33 +0200

    node:installed mocha

---
+++ node/test/test.js
@@ -0,0 +1,18 @@
+var chai = require('chai');  
+var assert = chai.assert;    // Using Assert style
+var expect = chai.expect;    // Using Expect style
+var should = chai.should;  // Using Should style
+
+
+
+
+
+assert(true);
+expect(true);
+should(true);
+//assert(false)
+
+expect(false)
+
+
+
