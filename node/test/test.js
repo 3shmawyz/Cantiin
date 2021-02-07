cdbede1283ae47f7b04fc28ccb1a3d0f33be1620
OmarThinks <contact.parmag@gmail.com>
2021-02-07 15:01:52 +0200

    test.js:add:created frst node test file

--- node/test/test.js
+++ node/test/test.js
@@ -1,18 +1,15 @@
-var chai = require('chai');  
-var assert = chai.assert;    // Using Assert style
-var expect = chai.expect;    // Using Expect style
-var should = chai.should;  // Using Should style
-
-
-
-
-
-assert(true);
-expect(true);
-should(true);
-//assert(false)
-
-expect(false)
-
-
-
+describe("test", ()=>
+{
+	describe("test1",()=>{
+		assert.equal(1, 1);
+		console.log("tst1")
+	})
+	describe("test3",()=>{
+		assert.equal(3, 3);
+		console.log("tst3")
+	})
+	describe("test2",()=>{
+		assert.equal(2, 2);
+		console.log("tst2")
+	})
+})
