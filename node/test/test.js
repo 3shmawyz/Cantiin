0660b388bb637fe7a9b04c76fe12ada10542765d
OmarThinks <contact.parmag@gmail.com>
2021-02-07 15:25:10 +0200

    validation_test.js:add:preparing

--- node/test/test.js
+++ node/test/test.js
@@ -1,16 +1,9 @@
 var {assert} = require("chai")
-describe("test", ()=>
+
+describe("Unit_testing", ()=>
 {
-	describe("test1",()=>{
+	describe("Unit_testing",()=>{
 		assert.equal(1, 1);
-		console.log("tst1")
-	})
-	describe("test3",()=>{
-		assert.equal(3, 3);
-		console.log("tst3")
-	})
-	describe("test2",()=>{
-		assert.equal(2, 2);
-		console.log("tst2")
+		console.log("Unit_testing, Hello!")
 	})
 })
