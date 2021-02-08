1eeae963eb83be93a2c61c634f64bc7366f2e307
OmarThinks <contact.parmag@gmail.com>
2021-02-08 02:23:01 +0200

    node:package scripts

---
+++ node/test/02_validation/test.js
@@ -0,0 +1,20 @@
+var {assert} = require("chai")
+var {Schema } = require("validate")
+
+/*var validation = require("./validation.js")*/
+
+describe("test", ()=>
+{
+	describe("test1",()=>{
+		assert.equal(1, 1);
+		console.log("tst1")
+	})
+	describe("test3",()=>{
+		assert.equal(3, 3);
+		console.log("tst3")
+	})
+	describe("test2",()=>{
+		assert.equal(2, 2);
+		console.log("tst2")
+	})
+})
