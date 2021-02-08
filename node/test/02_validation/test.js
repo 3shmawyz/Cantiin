48a7aa7d47436994398e1ab7439ced79e5c7ca98
OmarThinks <contact.parmag@gmail.com>
2021-02-08 02:27:11 +0200

    test2:add:first validation

--- node/test/02_validation/test.js
+++ node/test/02_validation/test.js
@@ -1,7 +1,51 @@
 var {assert} = require("chai")
-var {Schema } = require("validate")
+var {Schema} = require("validate")
 
-/*var validation = require("./validation.js")*/
+
+const user = new Schema({
+	username: 
+	{
+		type: String,
+		required: true,
+		length: { min: 3, max: 32 }
+	},
+	pets: 
+	[
+		{
+			name: 
+			{
+	  			type: String
+	  			required: true
+			},
+			animal: 
+			{
+	  			type: String
+	  			enum: ['cat', 'dog', 'cow']
+			}
+  		}
+  	],
+	address: 
+	{
+		street: 
+		{
+			type: String,
+			required: true
+		},
+		city: 
+		{
+			type: String,
+			required: true
+		}
+		zip: 
+		{
+	  		type: String,
+	  		match: /^[0-9]+$/,
+		required: true
+		}
+  	}
+})
+
+
 
 describe("test", ()=>
 {
