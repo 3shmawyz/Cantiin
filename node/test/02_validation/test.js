789d9616b16be58b7eb3317e049785a85b2c3807
OmarThinks <contact.parmag@gmail.com>
2021-02-08 04:44:13 +0200

    node:installed avg

--- node/test/02_validation/test.js
+++ node/test/02_validation/test.js
@@ -1,10 +1,12 @@
 //var {assert} = require("chai")
 //var validate = require("validate")
 //var Schema = validate.Schema
-import Schema from 'validate'
+//import Schema from 'validate'
 /*
 import Schema from 'validate'
 */
+
+/*
 const user = new Schema({
 	username: 
 	{
@@ -47,7 +49,7 @@ const user = new Schema({
 		}
   	}
 })
-
+*/
 
 /*
 describe("validate", ()=>
@@ -62,3 +64,63 @@ describe("validate", ()=>
 	})
 })
 */
+
+
+
+var Validator = require('validatorjs');
+
+/*
+
+let data = {
+  name: 'John',
+  email: 'johndoe@gmail.com',
+  age: 28
+};
+
+let rules = {
+  name: 'required',
+  email: 'required|email',
+  age: 'min:18'
+};
+
+let validation = new Validator(data, rules);
+
+validation.passes(); // true
+validation.fails(); // false
+
+vali = validation.errors.first('email'); // 'The email format is invalid.'
+errs = validation.errors.get('email'); 
+// returns an array of all email error messages
+
+console.log("Hello")
+
+*/
+
+
+
+
+let input = { name: '', email: '' };
+let rules = { name : 'required', email : 'required' };
+
+let validation = new Validator(input, rules);
+
+validation.passes();
+vali = validation.errors.first('name'); // returns  'The name field is required.'
+errs = validation.errors.first('email'); // returns 'Without an email we can\'t reach you!'
+
+console.log(typeof(validation.errors))
+console.log((validation.errors))
+console.log((validation))
+
+
+
+
+console.log(validation)
+console.log(errs)
+console.log("______________________")
+
+input = {name:1,email:"abc"}
+let validation1 = new Validator(input, rules);
+console.log(validation1)
+
+
