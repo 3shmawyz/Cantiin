6c93b9390405ef1c2c88260a0ad3bc6ad86097e4
Omar Magdy <contact.parmag@gmail.com>
2021-02-17 09:11:37 +0200

    __init__.py:mod:updated imports

--- deprecated/resrc/__init__.py
+++ deprecated/resrc/__init__.py
@@ -1,17 +1,9 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+SECRET="change me"
 
+from datetime import timedelta
+import secrets
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-"""db = SQLAlchemy()
-app = Flask("Cantiin")
-SQLALCHEMY_DATABASE_URI = "sqlite:///databases/test.sqlite"
-SQLALCHEMY_TRACK_MODIFICATIONS= False"""
 
 
-#from models import User, Product, Order, Image
-
-
-
-#Base.metadata.drop_all(bind=engine)
-#Base.metadata.create_all(bind=engine)
+EXPIRATION_AFTER= timedelta(days=7)
+db = SQLAlchemy()
