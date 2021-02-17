5353c9610148eed6ed8852c3d86fe23eea730d3d
Omar Magdy <contact.parmag@gmail.com>
2021-02-17 14:00:24 +0200

    app.py:add:error handlers

--- deprecated/resrc/app.py
+++ deprecated/resrc/app.py
@@ -45,14 +45,9 @@ def create_app(DOCKER=False,testing=TESTING):
 	if DOCKER:
 		app.config.from_object(config_docker)
 
-	#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databases/test.sqlite'
 	db.app = app
-
 	db.init_app(app)
-	#with app.app_context():
-	#    db.create_all()
 	db.create_all()
-	#return app
 
 	CORS(app,resources={r"*":{"origins":"*"}})
 	@app.after_request
@@ -63,7 +58,6 @@ def create_app(DOCKER=False,testing=TESTING):
 		response.headers.add("Access-Control-allow-Methods",
 			"GET,PUT,POST,DELETE,OPTIONS")
 		db.session.rollback()
-		#print("roll back", flush=True)
 		return response
 
 
@@ -141,5 +135,55 @@ def create_app(DOCKER=False,testing=TESTING):
 
 
 
+	@app.errorhandler(400)
+	def bad_request(error):
+		return jsonify({"success":False,"error":400,
+			"message":"bad request"}),400
+
+
+	@app.errorhandler(401)
+	def unauthorized(error):
+		return jsonify({"success":False,"error":401,
+			"message":"unauthorized"}),401
+
+
+	@app.errorhandler(403)
+	def forbidden(error):
+		return jsonify({"success":False,"error":403,
+			"message":"forbidden"}),403
+
+
+	@app.errorhandler(404)
+	def not_found(error):
+		return jsonify({"success":False,"error":404,
+			"message":"not found"}),404
+
+
+	@app.errorhandler(405)
+	def method_not_allowed(error):
+		return jsonify({"success":False,"error":405,
+			"message":"method not allowed"}),405
+
+
+	@app.errorhandler(422)
+	def unprocessible(error):
+		return jsonify({"success":False,"error":422,
+			"message":"unprocessible"}),422
+
+
+	@app.errorhandler(500)
+	def internal_server_error(error):
+		return jsonify({"success":False,"error":500,
+			"message":"internal server error"}),500
+
+
+
+	def test_only():
+		if testing == False:
+			abort(404)
+
 
 	return app
+
+if __name__ == '__main__':
+	create_app().run()
